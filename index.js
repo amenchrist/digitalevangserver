3de0989324f82875e720ac9dca8478653546443b
Amenchrist <amenphronesis@gmail.com>
2020-04-14 03:03:20 +0100

    connected to digital evang front end

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
 
+
+const app = express();
+
+app.use(cors());
+
+const user = {
+  name: "amenchrist",
+  class: "developer",
+  level: 1
+}
+
+app.get('/',(req, res) => res.send("Server connected Digital Evangelism"));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+/*
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
